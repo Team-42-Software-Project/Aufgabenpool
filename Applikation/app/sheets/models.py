32020b4724cd387c0d591328f704b50000063d5b
giselaphillips <gisela.phillips@unifr.ch>
2021-03-13 21:06:37 +0100

    Grundfunktionalität Aufgabenblatt erstellen

---
+++ Applikation/app/sheets/models.py
@@ -0,0 +1,14 @@
+from app import db
+from datetime import datetime 
+
+class Sheet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject= db.Column(db.String(100), nullable=False, default='N/A')
+    topic =db.Column(db.String(200), nullable=False, default='N/A')
+    text =db.Column(db.Text,nullable=True, default='N/A')
+    level=db.Column(db.Integer, nullable=True, default='N/A')
+    date_posted=db.Column(db.DateTime, nullable=True, default=datetime.utcnow)
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return 'Aufgabenblatt  '( + str(self.id)+ str(self.subject)+ str(self.topic))
