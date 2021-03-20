95cfbbd8021f98f4da4f98f4f362dfe1c335f7af
AnnetteMueller <anette.mueller@unifr.ch>
2021-03-20 10:41:45 +0100

    Implementation von Aufgaben ändern und löschen. Für das Ändern verwende ich das gleiche Formular wie für neue Aufgaben. Ausserdem habe ich den Fehler behoben, dass bei Themen wieder nur das erste Wort übernommen wurde.

--- Applikation/app/posts/views.py
+++ Applikation/app/posts/views.py
@@ -1,5 +1,6 @@
 import os
 import secrets
+import sys
 from PIL import Image
 from flask import Blueprint, render_template, request, redirect, url_for, flash, abort
 from app import app, db, bcrypt 
@@ -35,7 +36,31 @@ def entries():
        postlist = Post.query.filter_by(user_id=current_user.id).all()
        return render_template('posts/entries.html', posts=postlist)
 
+@postmod.route('/entries/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    post= Post.query.get_or_404(id)
+    form =PostForm(subject=post.subject, topic=post.topic, level=post.level, text=post.text)
+    if form.validate_on_submit():
+        post.subject=form.subject.data
+        post.topic= form.topic.data
+        post.level=form.level.data
+        post.text=form.text.data
+        
+        db.session.commit()
+        flash('Aufgabe geändert', 'success')
+        return redirect(url_for('posts.entries'))
+    topiclist = Topic.query.filter_by(user_id=current_user.id).all()
+    return render_template('posts/new_post.html', title= 'Aufgabe ändern', form=form, topiclist=topiclist, legend='Aufgabe ändern')
 
+
+@postmod.route('/entries/delete/<int:id>')
+@login_required
+def delete (id):
+    post= Post.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts/entries/')
 
 
 
