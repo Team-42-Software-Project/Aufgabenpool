2616d661794cbf3d42c4aedd1dd2af0879ab2fb3
giselaphillips <gisela.phillips@unifr.ch>
2021-03-30 20:57:36 +0200

    Navigation

--- Applikation/app/users/views.py
+++ Applikation/app/users/views.py
@@ -44,7 +44,6 @@ def login():
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page= request.args.get('next')
-            flash(f'Sie sind eingeloggt', 'success')
             return redirect(next_page) if next_page else redirect(url_for('home2'))
         else:
             flash(f'Ckeck email and password!', 'danger') 
